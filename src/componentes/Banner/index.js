05fb625efa35334fa1026224dc7186b53fab61d7
Renan Rossini <107894306+RehRossini@users.noreply.github.com>
2022-12-12 20:09:40 -0300

    Add files via upload

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,15 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX - Parte visual do React
+    return(
+        <header className='banner'>
+            <img src="/img/banner.png" alt="Banner da Organo"/>
+        </header>
+        
+    )
+}
+// Função que está sendo exportada
+
+// Nota: Sempre usar className para classe em HTML
+export default Banner
